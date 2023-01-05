4e2578626fc843aef732500953b26a6c1fe28cc2
matthewrhdu <matthew.du@mail.utoronto.ca>
2023-01-04 21:37:28 -0500

    Fiexed font size issue

--- src/images/convert.py
+++ src/images/convert.py
@@ -1,6 +1,16 @@
-import cv2
+import cv2 as cv
+import numpy as np
 
-file = "image0.jpeg"
-cv2.imread(file)
+file = "MusicBanner.png"
+img = cv.imread(file)
 
+img = cv.resize(img, None, fx=2, fy=2)
+blur = cv.GaussianBlur(img, (3, 3), 0)
+
+img = cv.addWeighted(img, 10, blur, -9, 1, blur)
+
+cv.imshow("img", img)
+cv.waitKey()
+
+cv.imwrite("MusicBanner_new.png", img)
 
