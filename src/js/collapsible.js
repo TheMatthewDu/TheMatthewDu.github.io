3fdfc5b385041d807156c39f12caf5829a315163
matthewrhdu <matthew.du@mail.utoronto.ca>
2023-08-07 01:37:42 -0400

    Updated site

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -1,14 +1,23 @@
 const coll = document.getElementsByClassName("collapsible");
-let i;
 
+function click_listener_func() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+
+    const tag = this.children[0];
+    if (tag.classList.contains("fa-caret-right")) {
+        tag.classList.replace("fa-caret-right", "fa-caret-down");
+    } else {
+        tag.classList.replace("fa-caret-down", "fa-caret-right");
+    }
+}
+
+let i;
 for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
+    coll[i].addEventListener("click", click_listener_func);
 }
