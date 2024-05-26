5c12e24bb4257cc006ca2ac49446f943d9bf1ce2
matthewrhdu <matthew.du@mail.utoronto.ca>
2024-05-26 00:05:43 -0400

    Added files

--- js/slide_control.js
+++ js/slide_control.js
@@ -1,17 +1,46 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
     }
-    x[slideIndex-1].style.display = "block";
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    if (slideIndex === 1){
+        slides[slideIndex - 1].style.display = "block";
+        slides[slides.length - 1].style.display = "block";
+    } else if (slideIndex === slides.length){
+        slides[slideIndex - 1].style.display = "block";
+        slides[0].style.display = "block";
+    }
+    else {
+        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 2].style.display = "block";
+    }
+    dots[slideIndex - 1].className += " active";
 }
